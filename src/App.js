9a83fd994585424c90da349851842b9f6cd44f48
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-14 14:09:08 +0100

    update index and app file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Reviews from "./components/Reviews/Reviews";
+import Homepage from "./components/Homepage/Homepage";
+import Navbar from "./components/Navbar/Navbar";
+import Categories from "./components/Categories/Categories";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/reviews/:review_id" />
+          <Route path="/reviews/:review_id/comments" />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
