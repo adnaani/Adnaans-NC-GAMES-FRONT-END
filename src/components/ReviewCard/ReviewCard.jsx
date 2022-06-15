6fb433454d408b8553d1b341d689ee763e6414b6
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-15 14:02:36 +0100

    make the card a link to the SingleCard

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -4,25 +4,28 @@ import * as BiIcons from "react-icons/bi";
 import * as FaIcons from "react-icons/fa";
 import { updateVotes } from "../../utils/api";
 import { organiseDate } from "../../utils/organiseDate";
+import { Link } from "react-router-dom";
 
 const ReviewCard = ({ review }) => {
   const [date, time] = organiseDate(review.created_at);
 
   return (
-    <li className="review-card">
-      <h3>{review.title}</h3>
-      <img
-        className="game-image"
-        src={review.review_img_url}
-        alt={review.title}
-      />
-      <h5>by {review.designer}</h5>
-      <span>Category: {review.category}</span>
-      <span>Votes: {review.votes}</span>
-      <BiIcons.BiUpvote className="vote-btn" />
-      <BiIcons.BiDownvote className="vote-btn" />
-      <FaIcons.FaInfoCircle className="info-btn" />
-    </li>
+    <Link className="review-link" to={`/reviews/${review.review_id}`}>
+      <li className="review-card">
+        <h3>{review.title}</h3>
+        <img
+          className="game-image"
+          src={review.review_img_url}
+          alt={review.title}
+        />
+        <h5>by {review.designer}</h5>
+        <span>Category: {review.category}</span>
+        <span>Votes: {review.votes}</span>
+        <BiIcons.BiUpvote className="vote-btn" />
+        <BiIcons.BiDownvote className="vote-btn" />
+        <FaIcons.FaInfoCircle className="info-btn" />
+      </li>
+    </Link>
   );
 };
 
