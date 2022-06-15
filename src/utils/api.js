5199192b3704c4e358bcffd0932234dbca1bb464
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-15 14:03:26 +0100

    update api

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,18 +4,24 @@ const gamesApi = axios.create({
   baseURL: "https://adnaans-games.herokuapp.com/api",
 });
 
-export const getAllCategories = () => {
-  return gamesApi.get("/categories").then(({ data }) => {
-    return data.categories;
-  });
-};
-
 export const getAllReviews = (category, sort_by, order) => {
   return gamesApi
     .get("/reviews", { params: { category, sort_by, order } })
     .then(({ data }) => {
       return data.reviews;
     });
+};
+
+export const getReviewById = (review_id) => {
+  return gamesApi.get(`reviews/${review_id}`).then(({ data }) => {
+    return data.reviews;
+  });
+};
+
+export const getAllCategories = () => {
+  return gamesApi.get("/categories").then(({ data }) => {
+    return data.categories;
+  });
 };
 
 export const updateVotes = () => {
