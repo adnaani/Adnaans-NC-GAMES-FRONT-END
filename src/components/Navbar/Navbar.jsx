d31d3250795b90a774e5130765dd37ad641d336c
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-17 12:07:38 +0100

    add user on nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import * as FaIcons from "react-icons/fa";
+import * as FiIcons from "react-icons/fi";
 import * as AiIcons from "react-icons/ai";
 import * as CgIcons from "react-icons/cg";
 import { IconContext } from "react-icons/";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
 import "./Navbar.css";
+import { UserContext } from "../../context/userContext";
 
 const Navbar = () => {
   const [sidebar, setSidebar] = useState(false);
-
+  const { loggedUser } = useContext(UserContext);
+  console.log(loggedUser);
   const showSidebar = () => setSidebar(!sidebar);
   return (
     <>
@@ -18,9 +21,26 @@ const Navbar = () => {
           <Link to="#" className="menu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
-          <Link to="/users">
-            <CgIcons.CgProfile className="profile" />
-          </Link>
+          {Object.keys(loggedUser).length === 0 ? (
+            <Link to="/users">
+              <CgIcons.CgProfile className="profile" />
+            </Link>
+          ) : (
+            <div className="logged-nav">
+              <section className="user-nav">
+                <img
+                  src={loggedUser.avatar_url}
+                  className="user-nav-image"
+                  alt={loggedUser.username}
+                />
+                <p>{loggedUser.username}</p>
+              </section>
+              <Link to="/users" className="select-user-nav">
+                <FiIcons.FiUsers />
+                Select User
+              </Link>
+            </div>
+          )}
         </div>
 
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
