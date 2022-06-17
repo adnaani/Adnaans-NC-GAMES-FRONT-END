a2a6efc3c28658094ed25d1c7fd3ffb239de3fd4
Cadnaan <c.adnaan-afrah@hotmail.com>
2022-06-17 11:40:23 +0100

    add comment form functionality

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,26 +1,30 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
 import "./CommentForm.css";
 const CommentForm = ({ handleSubmit, submitLabel }) => {
   const [body, setBody] = useState("");
-  const [author, setAuthor] = useState("");
+  // const [author, setAuthor] = useState("");
   const isTextareaDisabled = body.length === 0;
   const onSubmit = (event) => {
     event.preventDefault();
-    const input = { author, body };
+    const input = { author: loggedUser.username, body };
     handleSubmit(input);
     setBody("");
-    setAuthor("");
   };
+  const { loggedUser } = useContext(UserContext);
 
   return (
     <form onSubmit={onSubmit} className="comment-form-container">
-      <input
-        className="comment-form-input"
-        type="text"
-        value={author}
-        placeholder="enter username"
-        onChange={(e) => setAuthor(e.target.value)}
-      />
+      <section>
+        {Object.keys(loggedUser).length === 0 ? (
+          <Link to="/users">
+            <h3>Please Select a User</h3>
+          </Link>
+        ) : (
+          <h3>logged as: {loggedUser.username} </h3>
+        )}
+      </section>
       <textarea
         className="comment-form-textarea"
         value={body}
